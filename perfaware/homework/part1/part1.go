b2fd6652566dcd120d63ab1df9a2b5145430f269
Richard Graham <richard.graham@hexagon.com>
2025-09-29 23:13:17 +0200

    WIP: refactor to make everything op structs and clean up accumulated mess

--- perfaware/homework/part1/part1.go
+++ perfaware/homework/part1/part1.go
@@ -7,6 +7,150 @@ import (
 
 const DEBUG = false
 
+type Register struct {
+	name string
+	size uint8
+	offset uint8
+	fieldEncoding uint8
+}
+
+type Flags struct {
+	zero bool
+	sign bool
+}
+
+type EffectiveAddress struct {
+	 base string
+	 disp uint16
+	 dispSize uint8
+}
+
+func (ea EffectiveAddress) String() string {
+	if ea.base == "" {
+		return fmt.Sprintf("[%d]", ea.disp)
+	} else if ea.disp == 0 {
+		return fmt.Sprintf("[%s]", ea.base)
+	} else {
+		return fmt.Sprintf("[%s + %d]", ea.base, ea.disp)
+	}
+}
+
+
+type ArithmeticOpType = uint8
+const (
+	Add ArithmeticOpType = iota
+	NotSupported1
+	NotSupported2
+	NotSupported3
+	NotSupported4
+	Sub
+	NotSupported5
+	Cmp
+)
+
+type ArithmeticOp struct {
+	opType ArithmeticOpType
+	variant OperandVariant
+	wide bool
+	reg Register
+	otherReg Register
+	otherEffAddr EffectiveAddress
+	otherImmediate Immediate
+	size uint8
+}
+
+
+func (op ArithmeticOp) String() string {
+	var opMnemonic string
+	switch op.opType {
+		case Add:
+			opMnemonic = "add"
+		case Sub:
+			opMnemonic = "sub"
+		case Cmp:
+			opMnemonic = "cmp"
+		default:
+			opMnemonic = "NOT_SUPPORTED"
+	}
+	switch op.variant {
+		case REG_REG:
+			if op.dest {
+				return fmt.Sprintf("%s %s, %s", opMnemonic, op.reg.name, op.otherReg.name)
+			} else {
+				return fmt.Sprintf("%s %s, %s", opMnemonic, op.otherReg.name, op.reg.name)
+			}
+		case REG_MEM:
+			if op.dest {
+				return fmt.Sprintf("%s %s, %s", opMnemonic, op.reg.name, op.otherEffAddr.String())
+			} else {
+				return fmt.Sprintf("%s %s, %s", opMnemonic, op.otherEffAddr.String(), op.reg.name)
+			}
+		case REG_IMM:
+			return fmt.Sprintf("%s %s, %d", opMnemonic, op.reg.name, op.otherImmediate)
+		case MEM_IMM:
+			clarifier := ""
+			if op.wide {
+				clarifier = " word"
+			} else {
+				clarifier = " byte"
+			}
+			return fmt.Sprintf("%s%s %s, %d", opMnemonic, clarifier, op.otherEffAddr.String(), op.otherImmediate)
+		default:
+			panic("Unexpected Error")
+	}
+}
+
+
+
+type JmpOpType = uint8
+const (
+	JE JmpOpType = iota
+	JL
+	JLE
+	JB
+	JBE
+	JP
+	JO
+	JS
+	JNZ
+	JNL
+	JG
+	JNB
+	JA
+	JNP
+	JNO
+	JNS
+	LOOP
+	LOOPZ
+	LOOPNZ
+	JCXZ
+)
+
+type JumpOp struct {
+	opType JmpOpType
+	disp uint16
+	size uint8
+}
+
+func (op JumpOp) String() string {
+	return fmt.Sprintf("%s %d", getJumpOpName(op), op.disp)
+}
+
+func (op JumpOp) execute() {
+	switch (op.opType) {
+	case JNZ:
+		if !flagValues.zero {
+			// fmt.Printf("IP: %d, %x\n", ip, ip)
+			// fmt.Printf("disp: %d\n", int(op.disp))
+			ip += uint16(op.disp)
+			// fmt.Printf("IP: %d, %x\n", ip, ip)
+		}
+	default:
+		panic("Not implemented")
+	}
+}
+
+
 var RegMap = [16]Register{
 	{
 		name: "al",
@@ -117,41 +261,6 @@ var EffAddrBaseMap = [8]string{
 	"bx",
 }
 
-type ArithmeticOpType = uint8
-const (
-	Add ArithmeticOpType = iota
-	NotSupported1
-	NotSupported2
-	NotSupported3
-	NotSupported4
-	Sub
-	NotSupported5
-	Cmp
-)
-
-type JmpOpType = uint8
-const (
-	JE JmpOpType = iota
-	JL
-	JLE
-	JB
-	JBE
-	JP
-	JO
-	JS
-	JNZ
-	JNL
-	JG
-	JNB
-	JA
-	JNP
-	JNO
-	JNS
-	LOOP
-	LOOPZ
-	LOOPNZ
-	JCXZ
-)
 var jumpOpName = [20]string {
 	"je",
 	"jl",
@@ -179,56 +288,9 @@ func getJumpOpName(op JumpOp) string {
 	return jumpOpName[op.opType]
 }
 
-type JumpOp struct {
-	opType JmpOpType
-	disp uint16
-	size uint8
-}
-
-func (op JumpOp) String() string {
-	return fmt.Sprintf("%s %d", getJumpOpName(op), op.disp)
-}
 
 
-func getArithmeticOpName(op ArithmeticOpType) string {
-	switch op {
-	case Add:
-		return "add"
-	case Sub:
-		return "sub"
-	case Cmp:
-		return "cmp"
-	default:
-		return "NOT_SUPPORTED"
-	}
-}
-
- type EffectiveAddress struct {
-	 base string
-	 disp uint16
- }
-
- func (ea EffectiveAddress) String() string {
-	if ea.base == "" {
-		return fmt.Sprintf("[%d]", ea.disp)
-	} else if ea.disp == 0 {
-		return fmt.Sprintf("[%s]", ea.base)
-	} else {
-		return fmt.Sprintf("[%s + %d]", ea.base, ea.disp)
-	}
-}
-
-type Register struct {
-	name string
-	size uint8
-	offset uint8
-	fieldEncoding uint8
-}
-
-type Flags struct {
-	zero bool
-	sign bool
-}
+// CPU and Memory state
 
 var registerValues = [8]uint16{
 	0, // "ax",
@@ -247,6 +309,19 @@ var flagValues =  Flags {
 }
 
 var ip uint16;
+
+var memory = [65536]byte{}
+
+func load(memIdx int16) (byte, byte)  {
+	return memory[memIdx] , memory[memIdx+1 % 65536]
+}
+
+// func store(memIdx int16, , word bool) {
+// 	if word {
+// 	} else {
+// 	}
+// }
+
 
 func getRegisterValue(reg Register) uint16 {
 	if reg.size == 16 {
@@ -271,7 +346,7 @@ func updateRegister(reg Register,  value uint16) {
 	}
 }
 
-func executeOp(op ArithmeticOpType, dst Register, other uint16) {
+func executeArithmeticOp(op ArithmeticOp) {
 	current := getRegisterValue(dst)
 	var result uint16
 	switch op {
@@ -290,53 +365,51 @@ func executeOp(op ArithmeticOpType, dst Register, other uint16) {
 	flagValues.sign = result & 0x8000 > 0
 }
 
-
-
-func resolveOther(startIdx uint16, data []byte, mod byte, rm byte, w bool) (Register, EffectiveAddress, uint8) {
+func resolveOther(startIdx uint16, data []byte, mod byte, rm byte, w bool) (Register, EffectiveAddress) {
 	var otherReg Register
 	var otherEffAddr EffectiveAddress
-	var bytesConsumed uint8
 	effAddrBase := EffAddrBaseMap[rm]
 	switch mod {
-	case 0:
-		if rm == 6 {
+		case 0:
+			if rm == 6 {
+				dispLo := data[startIdx+2]
+				dispHi := data[startIdx+3]
+				disp := (uint16(dispHi) << 8) | uint16(dispLo)
+				otherEffAddr = EffectiveAddress{ base: "", disp: disp , dispSize: 2}
+			} else {
+				otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: 0, dispSize: 0}
+			}
+		case 1:
+			dispLo := uint16(data[startIdx+2])
+			otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: dispLo, dispSize: 1}
+		case 2:
 			dispLo := data[startIdx+2]
 			dispHi := data[startIdx+3]
 			disp := (uint16(dispHi) << 8) | uint16(dispLo)
-			otherEffAddr = EffectiveAddress{ base: "", disp: disp }
-			bytesConsumed = 4
-		} else {
-			otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: 0 }
-			bytesConsumed = 2
-		}
-	case 1:
-		dispLo := uint16(data[startIdx+2])
-		otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: dispLo }
-		bytesConsumed = 3
-	case 2:
-		dispLo := data[startIdx+2]
-		dispHi := data[startIdx+3]
-		disp := (uint16(dispHi) << 8) | uint16(dispLo)
-		otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: disp }
-		bytesConsumed = 4
-	case 3:
-		if w {
-			otherReg = RegMap[uint8(rm)+8]
-		} else {
-			otherReg = RegMap[uint8(rm)]
-		}
-		bytesConsumed = 2
-	default:
-		panic("MOD should never have this value")
+			otherEffAddr = EffectiveAddress{ base: effAddrBase, disp: disp, dispSize:2}
+		case 3:
+			if w {
+				otherReg = RegMap[uint8(rm)+8]
+			} else {
+				otherReg = RegMap[uint8(rm)]
+			}
+		default:
+			panic("MOD should never have this value")
 	}
-	return otherReg, otherEffAddr, bytesConsumed
+	return otherReg, otherEffAddr
 }
 
-func getImmediate(startIdx uint16, data []byte, wide bool) (uint16, uint8) {
+func getImmediate(startIdx uint16, data []byte, wide bool) Immediate {
 	if wide {
-		return uint16(uint16(data[startIdx+1])<<8 | uint16(data[startIdx])), 2
+		return Immediate{
+			uint16(uint16(data[startIdx+1])<<8 | uint16(data[startIdx])),
+			2,
+		}
 	} else {
-		return uint16(int8(data[startIdx])), 1
+		return Immediate{
+			uint16(int8(data[startIdx])),
+			1,
+		}
 	}
 }
 
@@ -348,7 +421,63 @@ func getOtherString(otherReg Register, otherEffAddr EffectiveAddress) string {
 	}
 }
 
-func acceptRegMemMov(startIdx uint16, data []byte) (string, uint) {
+type Immediate struct {
+	value uint16
+	size uint8
+}
+
+type OperandVariant = uint8
+const (
+	REG_REG OperandVariant = iota
+	REG_MEM
+	REG_IMM
+	MEM_REG
+	MEM_IMM
+)
+
+type MovOp struct {
+	variant OperandVariant
+	wide bool
+	reg Register
+	otherReg Register
+	effAddr EffectiveAddress
+	immediate Immediate
+	size uint8
+}
+
+func (op MovOp) String() string {
+	var src, dst string
+	switch (op.variant) {
+		case REG_REG:
+			dst = op.reg.name
+			src = op.otherReg.name
+		case REG_MEM:
+			dst = op.reg.name
+			src = op.effAddr.String()
+		case REG_IMM:
+			dst = op.reg.name
+			src = fmt.Sprintf("%d", op.immediate.value)
+		case MEM_REG:
+			dst = op.effAddr.String()
+			src = op.reg.name
+	}
+	return fmt.Sprintf("mov %s, %s", src, dst)
+}
+
+func (op MovOp) execute() {
+	switch (op.variant) {
+		case REG_REG:
+			updateRegister(op.reg, getRegisterValue(op.otherReg))
+		case REG_MEM:
+			panic("NOT YET")
+		case REG_IMM:
+			updateRegister(op.reg, op.immediate.value)
+		case MEM_REG:
+			panic("NOT YET")
+	}
+}
+
+func acceptRegMemMov(startIdx uint16, data []byte) MovOp {
 	d := data[startIdx]&2 > 0
 	w := data[startIdx]&1 > 0
 	b2 := data[startIdx+1]
@@ -357,43 +486,54 @@ func acceptRegMemMov(startIdx uint16, data []byte) (string, uint) {
 	regBits := (b2 & 0b00111000) >> 3
 
 	var reg Register
-	var bytesConsumed uint8
 	if w {
 		reg = RegMap[uint8(regBits)+8]
 	} else {
 		reg = RegMap[uint8(regBits)]
 	}
-	otherReg, otherEffAddr, bytesConsumed := resolveOther(startIdx, data, mod, rm, w)
-	var src, dst string
-	other := getOtherString(otherReg, otherEffAddr)
-	if d {
-		src = other
-		dst = reg.name
-	} else {
-		src = reg.name
-		dst = other
-	}
+	otherReg, otherEffAddr := resolveOther(startIdx, data, mod, rm, w)
+	var op MovOp
 	if otherReg.name !=  "" {
-		var current uint16
-		var dst Register
+		var dst, src Register
 		if d {
-			current = getRegisterValue(otherReg)
 			dst = reg
+			src = otherReg
 		} else {
-			current = getRegisterValue(reg)
 			dst = otherReg
+			src = reg
 		}
-		updateRegister(dst, current)
+		op = MovOp{
+			REG_REG,
+			w,
+			dst,
+			src,
+			EffectiveAddress{},
+			Immediate{},
+			2,
+		}
+	} else {
+		var variant = REG_MEM
+		if !d {
+			variant = MEM_REG
+		}
+		op = MovOp{
+			variant,
+			w,
+			reg,
+			Register{},
+			otherEffAddr,
+			Immediate{},
+			2 + otherEffAddr.dispSize,
+		}
 	}
-	asm := fmt.Sprintf("mov %s,%s", dst, src)
-	return asm, uint(bytesConsumed)
+	return op
 }
 
-func acceptImmediateToRegMov(startIdx uint16, data []byte) (string, uint) {
+func acceptImmediateToRegMov(startIdx uint16, data []byte) MovOp {
 	w := data[startIdx]&8 > 0
 	regBits := data[startIdx] & 0b00000111
 
-	immediate, immediateSize := getImmediate(startIdx+1, data, w)
+	immediate := getImmediate(startIdx+1, data, w)
 
 	var reg Register
 	if w {
@@ -401,11 +541,18 @@ func acceptImmediateToRegMov(startIdx uint16, data []byte) (string, uint) {
 	} else {
 		reg = RegMap[regBits]
 	}
-	updateRegister(reg, uint16(immediate))
-	return fmt.Sprintf("mov %s, %d", reg.name, immediate), uint(1 + immediateSize)
+	return MovOp{
+		REG_IMM,
+		w,
+		reg,
+		Register{},
+		EffectiveAddress{},
+		immediate,
+		immediate.size + 1,
+	}
 }
 
-func acceptArithmeticOpRegMem(startIdx uint16, data []byte) (string, uint) {
+func acceptArithmeticOpRegMem(startIdx uint16, data []byte) ArithmeticOp {
 	d := data[startIdx]&2 > 0
 	w := data[startIdx]&1 > 0
 	b2 := data[startIdx+1]
@@ -414,35 +561,53 @@ func acceptArithmeticOpRegMem(startIdx uint16, data []byte) (string, uint) {
 	rm := b2 & 0b00000111
 
 	var reg Register
-	var bytesConsumed uint8
 	if w {
 		reg = RegMap[uint8(regBits)+8]
 	} else {
 		reg = RegMap[uint8(regBits)]
 	}
-	op := uint8(data[startIdx] & 0b00111000 >> 3)
-	otherReg, otherEffAddr, bytesConsumed := resolveOther(startIdx, data, mod, rm, w)
-	other := getOtherString(otherReg, otherEffAddr)
-	var src, dst string
-	if d {
-		src = other
-		dst = reg.name
-		if otherReg.name != "" {
-			executeOp(op, reg, getRegisterValue(otherReg))
+	opType := uint8(data[startIdx] & 0b00111000 >> 3)
+	otherReg, otherEffAddr := resolveOther(startIdx, data, mod, rm, w)
+	var op ArithmeticOp
+	if otherReg.name !=  "" {
+		var dst, src Register
+		if d {
+			dst = reg
+			src = otherReg
+		} else {
+			dst = otherReg
+			src = reg
+		}
+		op = ArithmeticOp{
+			opType,
+			REG_REG,
+			w,
+			dst,
+			src,
+			EffectiveAddress{},
+			Immediate{},
+			2,
 		}
 	} else {
-		src = reg.name
-		dst = other
-		if otherReg.name != "" {
-			executeOp(op, otherReg, getRegisterValue(reg))
+		var variant = REG_MEM
+		if !d {
+			variant = MEM_REG
+		}
+		op = ArithmeticOp{
+			opType,
+			variant,
+			w,
+			reg,
+			Register{},
+			otherEffAddr,
+			Immediate{},
+			2 + otherEffAddr.dispSize,
 		}
 	}
-	asm := fmt.Sprintf("%s %s, %s", getArithmeticOpName(op), dst, src)
-	printFlags()
-	return asm, uint(bytesConsumed)
+	return op
 }
 
-func acceptArithmeticOpRegMemImmediate(startIdx uint16, data []byte) (string, uint) {
+func acceptArithmeticOpRegMemImmediate(startIdx uint16, data []byte) ArithmeticOp {
 	s := data[startIdx]&2 > 0
 	w := data[startIdx]&1 > 0
 	b2 := data[startIdx+1]
@@ -450,42 +615,59 @@ func acceptArithmeticOpRegMemImmediate(startIdx uint16, data []byte) (string, ui
 	rm := b2 & 0b00000111
 
 	var bytesConsumed uint8
-	dstReg, otherEffAddr, bytesConsumed := resolveOther(startIdx, data, mod, rm, w)
+	dstReg, otherEffAddr := resolveOther(startIdx, data, mod, rm, w)
 	wideImmediate := w && !s
-	immediate, imNumBytes := getImmediate(startIdx+uint16(bytesConsumed), data, wideImmediate)
-	bytesConsumed += imNumBytes
-	op := uint8(data[startIdx+1] & 0b00111000 >> 3)
-	clarifier := ""
-	if otherEffAddr.base != "" || otherEffAddr.disp != 0 {
-		if w {
-			clarifier = " word"
-		} else {
-			clarifier = " byte"
+	immediate := getImmediate(startIdx+uint16(bytesConsumed), data, wideImmediate)
+	opType := uint8(data[startIdx+1] & 0b00111000 >> 3)
+	var op ArithmeticOp
+	if dstReg.name !=  "" {
+		op = ArithmeticOp{
+			opType,
+			REG_IMM,
+			w,
+			dstReg,
+			Register{},
+			EffectiveAddress{},
+			immediate,
+			immediate.size + 2,
 		}
 	} else {
-		executeOp(op, dstReg, uint16(immediate))
+		op = ArithmeticOp{
+			opType,
+			MEM_IMM,
+			w,
+			Register{},
+			Register{},
+			otherEffAddr,
+			immediate,
+			immediate.size + otherEffAddr.dispSize + 2,
+		}
 	}
-	asm := fmt.Sprintf("%s%s %s, %d", getArithmeticOpName(op), clarifier, dstReg.name, immediate)
-	printFlags()
-	return asm, uint(bytesConsumed)
+	return op
 }
-//
-// func acceptArithmeticOpImmediateAcc(startIdx uint16, data []byte) (string, uint) {
-// 	w := data[startIdx]&1 > 0
-// 	immediate, _ := getImmediate(startIdx+1, data, w)
-// 	op := uint8(data[startIdx] & 0b00111000 >> 3)
-// 	var bytesConsumed uint
-// 	var reg string
-// 	if w {
-// 		reg = "ax"
-// 		bytesConsumed = 3
-// 	} else {
-// 		reg = "al"
-// 		bytesConsumed = 2
-// 	}
-// 	asm := fmt.Sprintf("%s %s, %d", ArithmeticVariantLookup[op], reg, immediate)
-// 	return asm, bytesConsumed
-// }
+
+func acceptArithmeticOpImmediateAcc(startIdx uint16, data []byte) ArithmeticOp {
+	w := data[startIdx]&1 > 0
+	immediate := getImmediate(startIdx+1, data, w)
+	opType := uint8(data[startIdx] & 0b00111000 >> 3)
+	var reg Register
+	regIdx := 0 // A register
+	if w {
+		reg = RegMap[regIdx + 8]
+	} else {
+		reg = RegMap[regIdx]
+	}
+	return ArithmeticOp{
+		opType,
+		REG_IMM,
+		w,
+		reg,
+		Register{},
+		EffectiveAddress{},
+		immediate,
+		immediate.size + 1,
+	}
+}
 
 func acceptCondJump(startIdx uint16, data []byte, opType JmpOpType) JumpOp {
 	displacement := uint16(int8(data[startIdx+1]))
@@ -507,22 +689,22 @@ func decode(filename string) {
 	streamSize := uint16(len(data))
 	for ip < streamSize {
 		var jmpOp JumpOp
-		var asm string
-		var bytesConsumed uint
+		var movOp MovOp
+		var arithmeticOp ArithmeticOp
 		if (0b10001000^data[ip])&0b11111100 == 0 {
-			asm, bytesConsumed = acceptRegMemMov(ip, data)
+			movOp = acceptRegMemMov(ip, data)
 		} else if (0b10110000^data[ip])&0b11110000 == 0 {
-			asm, bytesConsumed = acceptImmediateToRegMov(ip, data)
+			movOp = acceptImmediateToRegMov(ip, data)
 		} else if (0b00000000^data[ip])&0b11111100 == 0 ||
 			(0b00101000^data[ip])&0b11111100 == 0 ||
 			(0b00111000^data[ip])&0b11111100 == 0 {
-			asm, bytesConsumed = acceptArithmeticOpRegMem(ip, data)
+			arithmeticOp = acceptArithmeticOpRegMem(ip, data)
 		} else if (0b10000000^data[ip])&0b11111100 == 0 {
-			asm, bytesConsumed = acceptArithmeticOpRegMemImmediate(ip, data)
-		// } else if (0b00000100^data[ip])&0b11111110 == 0 ||
-		// 	(0b00101100^data[ip])&0b11111110 == 0 ||
-		// 	(0b00111100^data[ip])&0b11111110 == 0 {
-		// 	asm, bytesConsumed = acceptArithmeticOpImmediateAcc(ip, data)
+			arithmeticOp = acceptArithmeticOpRegMemImmediate(ip, data)
+		} else if (0b00000100^data[ip])&0b11111110 == 0 ||
+			(0b00101100^data[ip])&0b11111110 == 0 ||
+			(0b00111100^data[ip])&0b11111110 == 0 {
+			arithmeticOp = acceptArithmeticOpImmediateAcc(ip, data)
 		} else if data[ip] == 0b01110100 {
 			jmpOp = acceptCondJump(ip, data, JE)
 		} else if data[ip] == 0b01111100 {
@@ -568,30 +750,33 @@ func decode(filename string) {
 			panic("That wasn't supposed to happen")
 		}
 
+		var bytesConsumed uint
 		if jmpOp.size != 0 {
 			fmt.Println(jmpOp.String())
+			bytesConsumed += uint(jmpOp.size)
+		} else if movOp.size != 0 {
+			fmt.Println(movOp.String())
+			bytesConsumed += uint(movOp.size)
+		} else if arithmeticOp.size != 0 {
+			fmt.Println(arithmeticOp.String())
+			bytesConsumed += uint(arithmeticOp.size)
 		} else {
-			fmt.Println(asm)
-		}
-		if jmpOp.size != 0 {
-			bytesConsumed = uint(jmpOp.size)
+			fmt.Printf("%b\n", data[ip])
+			panic(fmt.Sprintf("Unmatched op: %b\n", data[ip]))
 		}
 		ip += uint16(bytesConsumed)
-		fmt.Printf("Bytes consumed: %d\n", bytesConsumed)
+		// fmt.Printf("Bytes consumed: %d\n", bytesConsumed)
 		if jmpOp.size != 0 {
-			switch (jmpOp.opType) {
-			case JNZ:
-				if !flagValues.zero {
-					fmt.Printf("IP: %d, %x\n", ip, ip)
-					fmt.Printf("disp: %d\n", int(jmpOp.disp))
-					ip += uint16(jmpOp.disp)
-					fmt.Printf("IP: %d, %x\n", ip, ip)
-				}
-			default:
-				panic("Not implemented")
-			}
+			jmpOp.execute()
+		} else if movOp.size != 0 {
+			movOp.execute()
+		} else if arithmeticOp.size != 0 {
+			arithmeticOp.execute()
+		} else {
+			fmt.Printf("%b\n", data[ip])
+			panic(fmt.Sprintf("Unmatched op: %b\n", data[ip]))
 		}
-		fmt.Printf("IP: %d\n", ip)
+		// fmt.Printf("IP: %d\n", ip)
 	}
 
 }
